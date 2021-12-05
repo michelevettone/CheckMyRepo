190f7ba02a581f0155284613f36b1e67c0222afa
michelevettone <63918547+michelevettone@users.noreply.github.com>
2021-12-05 12:12:20 +0100

    Initial commit

---
+++ src/view/MainView.tsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react';
+import { Text, View } from 'react-native';
+
+export const MainView: React.FC = () => {
+    return (
+        <View style={{width: '100%', height: '100%', backgroundColor: 'red'}}>
+            <Text>prova</Text>
+        </View>
+    );
+};
